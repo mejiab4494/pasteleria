97d4fc8dbe4fdcc21073956e29dc2791a7039c90
Bryan Mejia <mejiab2@gmail.com>
2026-07-07 21:54:15 -0400

    updated postres, cakes, and ubicaion

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const pasteles = document.querySelector('.pasteles');
+const pasteles = document.querySelector(".pasteles");
+const postres = document.querySelector(".postres");
 
 const cakeImages = [
     "cakemama.jpg",
     "plantas.jpg",
     "tresLeches.jpg",
-]
+    "pasteles1.jpg",
+    "pasteles2.jpg",
+    "pasteles3.jpg",
+];
 
-const cakesHTML = cakeImages.map((img) => {
-    return `
+const postreImages = [
+    "cheesecake.jpg",
+    "chocoflan.jpg",
+    "cupbakes.jpg",
+];
+
+if (pasteles) {
+    const cakesHTML = cakeImages.map(img => `
         <div class="pastel-card">
             <img src="images/cakes/${img}" alt="Pastel">
         </div>
-    `
-})
-.join('');
+    `).join("");
 
-pasteles.innerHTML = cakesHTML;
+    pasteles.innerHTML = cakesHTML;
+}
+
+if (postres) {
+    const postresHTML = postreImages.map(img => `
+        <div class="postre-card">
+            <img src="images/postres/${img}" alt="Postre">
+        </div>
+    `).join("");
+
+    postres.innerHTML = postresHTML;
+}
